1a60c5f4a5762f6d9aa9b5d8a877d76824a5ffb5
yaseenbabamohammad <babayaseen0786@gmail.com>
2025-01-04 11:49:29 +0530

    final Commit

--- Corporate Allocation/build.sh
+++
@@ -1,9 +0,0 @@
-cd EquipmentAllocation
-docker build -t scala-play-corporate-equipment-allocation:latest .
-
-cd ..
-cd NotificationService
-docker build -t notification-service:latest .
-
-cd ..
-docker network create kafka-network
